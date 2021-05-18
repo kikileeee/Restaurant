5093204f1cb807987a3416d1e0fec7a7971afc6a
kikileeee <paulovpdv@gmail.com>
2021-05-18 23:03:24 +0200

    added html structure

---
+++ main.js
@@ -0,0 +1,4 @@
+$.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=AIzaSyBA3D06DHZynb7XNzg0rkny4JHuLYaxBIU', function(data){
+
+    console.log(data)
+})
